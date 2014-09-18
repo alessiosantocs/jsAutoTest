e1dc1d940221f922059190560a36d105bc4658e0
Alessio Santo <alessio.santocs@gmail.com>
2014-09-18 09:29:21 +0200

    Serving static assets on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Crawler::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
 
   # Specifies the header that your server uses for sending files.
